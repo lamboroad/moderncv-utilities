a2da7b88adde5592858c7eec06afe0d9d4262e4f
mathieu <lamboroad@gmail.com>
2026-02-22 17:02:44 +0100

    fix script bash.

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -3,8 +3,8 @@
 
 # Usage info
 show_help() {
-cat << EOF
-Usage: ${0##*/} [-hv] [-t OUTPUT_FORMAT] [-o OUTPUT_DIR] [TEX_FILEPATH]...
+  cat <<EOF
+Usage: ${0##*/} [-hv] [-t OUTPUT_FORMAT] [-o OUTPUT_DIR] -- [TEX_FILEPATH]...
 Convert moderncv latex file to the desired format and write the result to standard output.
 
       -h|--help                         display help.
@@ -16,8 +16,8 @@ EOF
 }
 
 die() {
-    printf '%s\n' "$1" >&2
-    exit 1
+  printf '%s\n' "$1" >&2
+  exit 1
 }
 
 # Initialize all the option variables.
@@ -28,56 +28,58 @@ output_directory=$(pwd)
 verbose=0
 
 while :; do
-    case $1 in
-        -h|-\?|--help)
-            show_help
-            exit
-            ;;
-        -t|--outputformat)
-            if [ "$2" ]; then
-                output_format=$2
-            else
-                die 'ERROR: "--outputformat" requires a non-empty option argument.'
-            fi
+  case $1 in
+  -h | -\? | --help)
+    show_help
+    exit
+    ;;
+  -t | --outputformat)
+    if [ "$2" ]; then
+      output_format=$2
+      shift
+    else
+      die 'ERROR: "--outputformat" requires a non-empty option argument.'
+    fi
 
-            if [[ $output_format != "pdf" && $output_format != "dvi" && $output_format != "ps" ]]; then
-                die 'ERROR: "--outputformat" requires a value of [dvi|pdf|ps].'
-            fi
-            ;;
-        -o|--outputdirectory)
-            if [ "$2" ] && [ -d "$DIRECTORY" ]; then
-                output_directory=$2
-            else
-                die 'ERROR: "--outputdirectory" requires a non-empty option argument and an existing directory.'
-            fi
-            ;;
-        -v|--verbose)
-            verbose=$((verbose + 1))  # Each -v adds 1 to verbosity. 
-            ;;
-        --)              # End of all options.
-            shift
-            break
-            ;;
-        -?*)
-            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
-            ;;
-        *) break         # Default case: No more options, so break out of the loop.
-    esac
+    if [[ $output_format != "pdf" && $output_format != "dvi" && $output_format != "ps" ]]; then
+      die 'ERROR: "--outputformat" requires a value of [dvi|pdf|ps].'
+    fi
+    ;;
+  -o | --outputdirectory)
+    if [ "$2" ] && [ -d "$2" ]; then
+      output_directory=$2
+      shift
+    else
+      die 'ERROR: "--outputdirectory" requires a non-empty option argument and an existing directory.'
+    fi
+    ;;
+  -v | --verbose)
+    verbose=$((verbose + 1)) # Each -v adds 1 to verbosity.
+    ;;
+  --) # End of all options.
     shift
+    break
+    ;;
+  -?*)
+    printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+    ;;
+  *) break ;; # Default case: No more options, so break out of the loop.
+  esac
+  shift
 done
 
 # arg 1: tex file
 if [[ -n $1 ]]; then
-    tex_filepath=$1
-    cp $tex_filepath $output_directory
-    tex_file="$(basename -- $tex_filepath)"
+  tex_filepath=$1
+  cp $tex_filepath $output_directory
+  tex_file="$(basename -- $tex_filepath)"
 else
-    die 'ERROR: "TEX_FILEPATH" is required.'
+  die 'ERROR: "TEX_FILEPATH" is required.'
 fi
 
 cmd_options="-$output_format"
 if [ $verbose -eq 0 ]; then
-    cmd_options="$cmd_options -quiet"
+  cmd_options="$cmd_options -quiet"
 fi
 
 # enable extglob which include the negation for the rm command
@@ -90,6 +92,8 @@ rm --force -- ${tex_filepath%.tex}.!(tex)
 # run docker image to convert latex file to pdf
 # sudo docker run --user="$(id -u):$(id -g)" -i -v "$(pwd):/tmp" latex latexmk -f $cmd_options -outdir=/tmp /tmp/$tex_filename.tex
 # echo "sudo docker run --user="$(id -u):$(id -g)" -i -v "$output_directory:/tmp" cv_builder miktex-pdflatex /tmp/$tex_file --quiet --output-directory="/tmp" --include-directory="/tmp" --output-format="$output_format""
+echo "docker run --user=$(id -u):$(id -g) -i -v $output_directory:/tmp cv_builder miktex-pdflatex /tmp/$tex_file --quiet --output-directory=/tmp --include-directory=/tmp --output-format=$output_format"
 sudo docker run --user="$(id -u):$(id -g)" -i -v "$output_directory:/tmp" cv_builder miktex-pdflatex /tmp/$tex_file --quiet --output-directory="/tmp" --include-directory="/tmp" --output-format="$output_format"
 # cleanup again but keep generated pdf
 rm --force -- ${tex_filepath%.tex}.!(tex|pdf|dvi|ps|log)
+
